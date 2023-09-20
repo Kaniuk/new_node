28d5c665482ba4c0d067668cf4ea65da081b5556
Liudmyla <lyudmula1989@ukr.net>
2023-09-20 11:17:15 +0200

    add dir and file creation with node:fs/promises

--- app.js
+++ app.js
@@ -1,80 +1,33 @@
-/*
-Створіть папку
-В тій папці створіть 5 папок і 5 файлів
-І за допомогою модулю fs виведіть в консоль, чи це папка чи це файл
-FILE: {fileName}
-FOLDER: {folderName}*/
-
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 
-const filePath = path.join(__dirname, 'mainFolder');
+const foo = async () => {
+    try {
+        const fileArr = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt',];
+        const folderArr = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5',];
 
-fs.mkdir(filePath, (err) => {
-    if (err) throw new Error(err.message);
-});
+        const basePath = path.join(process.cwd(), 'base-path');
 
-const firstFile = path.join(__dirname, 'mainFolder', 'firstText.txt');
-fs.writeFile(firstFile, 'Hello!!!', (err) => {
-    if (err) throw new Error(err.message);
-});
-const secondFile = path.join(__dirname, 'mainFolder', 'secondText.txt');
-fs.writeFile(secondFile, 'Hello!!!', (err) => {
-    if (err) throw new Error(err.message);
-});
-const thirdFile = path.join(__dirname, 'mainFolder', 'thirdText.txt');
-fs.writeFile(thirdFile, 'Hello!!!', (err) => {
-    if (err) throw new Error(err.message);
-});
-const fourthFile = path.join(__dirname, 'mainFolder', 'fourthText.txt');
-fs.writeFile(fourthFile, 'Hello!!!', (err) => {
-    if (err) throw new Error(err.message);
-});
-const fifthFile = path.join(__dirname, 'mainFolder', 'fifthText.txt');
-fs.writeFile(fifthFile, 'Hello!!!', (err) => {
-    if (err) throw new Error(err.message);
-});
+        await fs.mkdir(basePath, {recursive: true});
+        await Promise.all([
+            ...folderArr.map(async (name) => {
+                await fs.mkdir(path.join(basePath, name));
+            }),
+            ...fileArr.map(async (name) => {
+                await fs.writeFile(path.join(basePath, name), 'Hello!');
+            })
+        ]);
+        const arr = await fs.readdir(basePath);
+        for (const item of arr) {
+            const stat = await fs.stat(path.join(basePath, item));
+            console.log(stat.isFile() ? 'FILE -' : 'FOLDER -', item);
+        }
 
-const dir1 = path.join(__dirname, 'mainFolder','dir1');
-
-fs.mkdir(dir1, (err) => {
-    if (err) throw new Error(err.message);
-});
-
-const dir2 = path.join(__dirname, 'mainFolder','dir2');
-
-fs.mkdir(dir2, (err) => {
-    if (err) throw new Error(err.message);
-});
-
-const dir3 = path.join(__dirname, 'mainFolder','dir3');
-
-fs.mkdir(dir3, (err) => {
-    if (err) throw new Error(err.message);
-});
-
-const dir4 = path.join(__dirname, 'mainFolder','dir4');
-
-fs.mkdir(dir4, (err) => {
-    if (err) throw new Error(err.message);
-});
-
-const dir5 = path.join(__dirname, 'mainFolder','dir5');
-
-fs.mkdir(dir5, (err) => {
-    if (err) throw new Error(err.message);
-});
-
-
-fs.readdir(filePath, (err, files) => {
-    if (err)
-        throw new Error(err.message);
-    else {
-        console.log("\Filenames with the .txt extension:");
-        files.forEach(file => {
-            if (path.extname(file) === ".txt") {
-                console.log('this is file', file);
-            } else console.log('DIRRRR!!!',file)
-        })
+    } catch (e) {
+        console.log(e.message);
     }
-})
+
+};
+
+
+foo().then();
