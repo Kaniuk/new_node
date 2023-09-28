1126a487eb3ca56e9c85abda59131b5e39a829e1
Liudmyla <lyudmula1989@ukr.net>
2023-09-28 11:46:56 +0200

    Add joi

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,5 +3,8 @@ import { config } from "dotenv";
 config();
 
 export const configs = {
-  DB_URI: process.env.DB_URI,
+  DB_URI:
+    process.env.DB_URI ||
+    "mongodb+srv://NewNode:NewNode@newnode.5z3ciul.mongodb.net/",
+  PORT: process.env.PORT || 5001,
 };
